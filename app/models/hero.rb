ed1e6c9816442c406e0f14c3835933f3290e43ee
Karen Flores <karen.ag.flores@gmail.com>
2019-10-31 22:29:44 +0000

    Automatically backed up by Learn

---
+++ app/models/hero.rb
@@ -0,0 +1,15 @@
+class Hero
+  attr_reader :name, :power, :bio
+
+  @@heroes = []
+
+  def initialize(info)
+    @name = info[:name]
+    @power = info[:power]
+    @bio = info[:bio]
+    @@heroes << self
+  end
+
+  def self.all
+    @@heroes
+  end
