b4588248a7a0d80aa02a666e7c24f7a2acd3a1d3
Karen Flores <karen.ag.flores@gmail.com>
2019-10-31 22:32:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
     end
 
     post 'teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:heroes].each do |details|
+        Hero.new(details)
+      end
+      @heroes = Hero.all
+      
       erb :teams
     end
 end
